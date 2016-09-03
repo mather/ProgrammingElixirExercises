cb3edec846a624ea491a452a572a63ce604a3a2d
mather <mather314@gmail.com>
2016-09-03 16:26:51 +0900

    Chap5 Ex2,3

--- test/chapter5_test.exs
+++ test/chapter5_test.exs
@@ -12,4 +12,33 @@ defmodule Chapter5Test do
     assert pair_tuple_to_list.({1234, 5678}) == [1234, 5678]
   end
 
+  test "Functions - 2" do
+    fizzbuzz = fn
+      (0, 0, _) -> "FizzBuzz"
+      (0, _, _) -> "Fizz"
+      (_, 0, _) -> "Buzz"
+      (_, _, n) -> n
+    end
+
+    assert fizzbuzz.(0, 0, 15) == "FizzBuzz"
+    assert fizzbuzz.(0, 1, 6)  == "Fizz"
+    assert fizzbuzz.(1, 0, 10) == "Buzz"
+    assert fizzbuzz.(1, 3, 13) == 13
+  end
+
+  test "Functions - 3" do
+    fizzbuzz = fn
+      (0, 0, _) -> "FizzBuzz"
+      (0, _, _) -> "Fizz"
+      (_, 0, _) -> "Buzz"
+      (_, _, n) -> n
+    end
+
+    fizzbuzz_n = fn n -> fizzbuzz.(rem(n, 3), rem(n, 5), n) end
+
+    assert fizzbuzz_n.(15) == "FizzBuzz"
+    assert fizzbuzz_n.(6)  == "Fizz"
+    assert fizzbuzz_n.(10) == "Buzz"
+    assert fizzbuzz_n.(13) == 13
+  end
 end
