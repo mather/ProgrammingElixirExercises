2c7274d323ecdf93e39a92958b82ffdff8fa9aa3
mather <mather314@gmail.com>
2016-09-03 15:58:36 +0900

    Chap2 Ex3

--- test/chapter2_test.exs
+++ test/chapter2_test.exs
@@ -25,4 +25,33 @@ defmodule Chapter2Test do
     [a, b, a] = [1, 2, 1]
     assert {a, b} == {1, 2}
   end
+
+  test "PatternMatching - 3" do
+    assert_raise MatchError, fn ->
+      a = 2
+      [a, b, a] = [1, 2, 3]
+    end
+
+    assert_raise MatchError, fn ->
+      a = 2
+      [a, b, a] = [1, 1, 2]
+    end
+
+    a = 2
+    a = 1
+    assert a == 1
+
+    a = 2
+    ^a = 2
+
+    assert_raise MatchError, fn ->
+      a = 2
+      ^a = 1
+    end
+
+    assert_raise MatchError, fn ->
+      a = 2
+      ^a = 2 - a
+    end
+  end
 end
